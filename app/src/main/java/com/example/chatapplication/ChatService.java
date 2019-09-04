6da34772768fe8938b62dc2e4a8b0dddd9d987f8
Sunah Gong <glglthssla@naver.com>
2019-09-04 17:42:45 +0900

    - ChatService 생성

---
+++ app/src/main/java/com/example/chatapplication/ChatService.java
@@ -0,0 +1,31 @@
+package com.example.chatapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ChatService extends Service {
+    public ChatService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
